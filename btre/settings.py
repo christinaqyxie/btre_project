3cd195e7a1427a81789ff083f2fd6e04217f5f97
Christina <christinaqyxie@gmail.com>
2021-05-11 18:05:12 -0400

    Removed email from settings

--- btre/settings.py
+++ btre/settings.py
@@ -142,12 +142,6 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger', 
 
 }
-# Email config
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER='tina.christina.xie@gmail.com'
-EMAIL_HOST_PASSWORD='336300Lavender12345'
-EMAIL_USE_TLS=True
 
 
 try:
